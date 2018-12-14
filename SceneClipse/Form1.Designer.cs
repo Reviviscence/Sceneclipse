8c11506a003ceb392d59ce319a678abc7d2a619d
th.kwon29@gmail.com <th.kwon29@gmail.com>
2018-12-14 22:05:01 +0900

    Initial commit
    - Movie player feature using Windows Media Player Object
    - Added feature for bookmarking and making thumbnails
    - Added feature for jump into bookmarked time by clicking

---
+++ SceneClipse/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace SceneClipse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.axMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.labelTimeDisplay = new System.Windows.Forms.Label();
+            this.timerCurrentTime = new System.Windows.Forms.Timer(this.components);
+            this.buttonPause = new System.Windows.Forms.Button();
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.textBoxOpenFileName = new System.Windows.Forms.TextBox();
+            this.labelPlayTime = new System.Windows.Forms.Label();
+            this.labelOpenFileName = new System.Windows.Forms.Label();
+            this.timerPlayTime = new System.Windows.Forms.Timer(this.components);
+            this.buttonBookmark = new System.Windows.Forms.Button();
+            this.listViewBookmark = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "현재 시간 : ";
+            // 
+            // axMediaPlayer1
+            // 
+            this.axMediaPlayer1.Enabled = true;
+            this.axMediaPlayer1.Location = new System.Drawing.Point(177, 108);
+            this.axMediaPlayer1.Name = "axMediaPlayer1";
+            this.axMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMediaPlayer1.OcxState")));
+            this.axMediaPlayer1.Size = new System.Drawing.Size(611, 426);
+            this.axMediaPlayer1.TabIndex = 1;
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(14, 510);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 2;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(14, 481);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(75, 23);
+            this.buttonPlay.TabIndex = 3;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // labelTimeDisplay
+            // 
+            this.labelTimeDisplay.AutoSize = true;
+            this.labelTimeDisplay.Location = new System.Drawing.Point(14, 121);
+            this.labelTimeDisplay.Name = "labelTimeDisplay";
+            this.labelTimeDisplay.Size = new System.Drawing.Size(0, 12);
+            this.labelTimeDisplay.TabIndex = 4;
+            // 
+            // timerCurrentTime
+            // 
+            this.timerCurrentTime.Enabled = true;
+            this.timerCurrentTime.Interval = 1000;
+            this.timerCurrentTime.Tick += new System.EventHandler(this.timerCurrentTime_Tick);
+            // 
+            // buttonPause
+            // 
+            this.buttonPause.Location = new System.Drawing.Point(96, 481);
+            this.buttonPause.Name = "buttonPause";
+            this.buttonPause.Size = new System.Drawing.Size(75, 23);
+            this.buttonPause.TabIndex = 5;
+            this.buttonPause.Text = "Pause";
+            this.buttonPause.UseVisualStyleBackColor = true;
+            this.buttonPause.Click += new System.EventHandler(this.buttonPause_Click);
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Location = new System.Drawing.Point(14, 452);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpenFile.TabIndex = 6;
+            this.buttonOpenFile.Text = "파일 열기";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            this.buttonOpenFile.Click += new System.EventHandler(this.buttonOpenFile_Click);
+            // 
+            // textBoxOpenFileName
+            // 
+            this.textBoxOpenFileName.Location = new System.Drawing.Point(16, 178);
+            this.textBoxOpenFileName.MaximumSize = new System.Drawing.Size(150, 500);
+            this.textBoxOpenFileName.MinimumSize = new System.Drawing.Size(150, 4);
+            this.textBoxOpenFileName.Multiline = true;
+            this.textBoxOpenFileName.Name = "textBoxOpenFileName";
+            this.textBoxOpenFileName.ReadOnly = true;
+            this.textBoxOpenFileName.ShortcutsEnabled = false;
+            this.textBoxOpenFileName.Size = new System.Drawing.Size(150, 36);
+            this.textBoxOpenFileName.TabIndex = 8;
+            // 
+            // labelPlayTime
+            // 
+            this.labelPlayTime.Location = new System.Drawing.Point(14, 336);
+            this.labelPlayTime.Name = "labelPlayTime";
+            this.labelPlayTime.Size = new System.Drawing.Size(100, 55);
+            this.labelPlayTime.TabIndex = 9;
+            // 
+            // labelOpenFileName
+            // 
+            this.labelOpenFileName.AutoSize = true;
+            this.labelOpenFileName.Location = new System.Drawing.Point(14, 147);
+            this.labelOpenFileName.Name = "labelOpenFileName";
+            this.labelOpenFileName.Size = new System.Drawing.Size(121, 12);
+            this.labelOpenFileName.TabIndex = 10;
+            this.labelOpenFileName.Text = "현재 재생중인 파일 : ";
+            // 
+            // timerPlayTime
+            // 
+            this.timerPlayTime.Interval = 1000;
+            this.timerPlayTime.Tick += new System.EventHandler(this.timerPlayTime_Tick);
+            // 
+            // buttonBookmark
+            // 
+            this.buttonBookmark.Location = new System.Drawing.Point(96, 510);
+            this.buttonBookmark.Name = "buttonBookmark";
+            this.buttonBookmark.Size = new System.Drawing.Size(75, 23);
+            this.buttonBookmark.TabIndex = 12;
+            this.buttonBookmark.Text = "책갈피";
+            this.buttonBookmark.UseVisualStyleBackColor = true;
+            this.buttonBookmark.Click += new System.EventHandler(this.buttonBookmark_Click);
+            // 
+            // listViewBookmark
+            // 
+            this.listViewBookmark.Location = new System.Drawing.Point(13, 236);
+            this.listViewBookmark.Name = "listViewBookmark";
+            this.listViewBookmark.Size = new System.Drawing.Size(153, 175);
+            this.listViewBookmark.TabIndex = 13;
+            this.listViewBookmark.UseCompatibleStateImageBehavior = false;
+            this.listViewBookmark.View = System.Windows.Forms.View.Details;
+            this.listViewBookmark.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.listViewBookmark_ItemSelectionChanged);
+            this.listViewBookmark.SelectedIndexChanged += new System.EventHandler(this.listViewBookmark_SelectedIndexChanged);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth16Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(64, 64);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(177, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(793, 540);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listViewBookmark);
+            this.Controls.Add(this.buttonBookmark);
+            this.Controls.Add(this.labelOpenFileName);
+            this.Controls.Add(this.labelPlayTime);
+            this.Controls.Add(this.textBoxOpenFileName);
+            this.Controls.Add(this.buttonOpenFile);
+            this.Controls.Add(this.buttonPause);
+            this.Controls.Add(this.labelTimeDisplay);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.axMediaPlayer1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private AxWMPLib.AxWindowsMediaPlayer axMediaPlayer1;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Label labelTimeDisplay;
+        private System.Windows.Forms.Timer timerCurrentTime;
+        private System.Windows.Forms.Button buttonPause;
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.TextBox textBoxOpenFileName;
+        private System.Windows.Forms.Label labelPlayTime;
+        private System.Windows.Forms.Label labelOpenFileName;
+        private System.Windows.Forms.Timer timerPlayTime;
+        private System.Windows.Forms.Button buttonBookmark;
+        private System.Windows.Forms.ListView listViewBookmark;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ SceneClipse/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SceneClipse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listViewBookmark.Columns.Add("Time", 50);
+            listViewBookmark.Columns.Add("dblTime", 0);
+            listViewBookmark.Columns.Add("Caption", 80);
+            listViewBookmark.LargeImageList = imageList1;
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            axMediaPlayer1.URL = @"c:\Wildlife.wmv";
+            timerPlayTime.Start();
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            axMediaPlayer1.Ctlcontrols.stop();
+        }
+
+        private void timerCurrentTime_Tick(object sender, EventArgs e)
+        {
+            string sCurrentTime = DateTime.Now.ToString("tt HH:mm:ss");
+            labelTimeDisplay.Text = sCurrentTime;
+        }
+
+        private void buttonPause_Click(object sender, EventArgs e)
+        {
+            if( axMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPaused )
+               axMediaPlayer1.Ctlcontrols.play();
+            else if( axMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPlaying )
+               axMediaPlayer1.Ctlcontrols.pause();
+        }
+
+        private void buttonOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFiledialog1 = new OpenFileDialog();
+            // openFiledialog1.Filter = "Movie files|*.wmv";
+
+            if( openFiledialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK )
+            {
+                axMediaPlayer1.URL = openFiledialog1.FileName;
+                textBoxOpenFileName.Text = openFiledialog1.FileName;
+                timerPlayTime.Start();
+
+                // labelOpenFileName.Text = "재생중인 파일 : " + openFiledialog1.FileName;
+            }
+
+            
+        }
+
+        private void timerPlayTime_Tick(object sender, EventArgs e)
+        {
+            if (axMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPlaying ||
+                axMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPaused)
+            {
+                string sVideoTime = axMediaPlayer1.Ctlcontrols.currentPositionString;
+                labelPlayTime.Text = "재생 중 : \n" + sVideoTime;
+            }
+            else
+            {
+                labelPlayTime.Text = "";
+                timerPlayTime.Stop();
+            }
+        }
+
+        private void buttonBookmark_Click(object sender, EventArgs e)
+        {
+
+            string sVideoTime = axMediaPlayer1.Ctlcontrols.currentPositionString;
+            double dVideoTime = axMediaPlayer1.Ctlcontrols.currentPosition;
+
+            // mediaplayer에서 이미지 가져오기
+            using (Bitmap bitmap = new Bitmap(axMediaPlayer1.Width, axMediaPlayer1.Height - 76))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(axMediaPlayer1.PointToScreen(new System.Drawing.Point()).X,
+                        axMediaPlayer1.PointToScreen(new System.Drawing.Point()).Y,
+                        0, 0,
+                        new System.Drawing.Size(
+                            axMediaPlayer1.Width, axMediaPlayer1.Height - 76));
+
+                }
+                // 이미지 표시(디버그용)                }
+                // bitmap.Save("e:\\test.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                // if (pictureBox1.Image != null) pictureBox1.Image.Dispose();
+                // pictureBox1.Image = bitmap.Clone(new Rectangle(0, 0, bitmap.Width, bitmap.Height), System.Drawing.Imaging.PixelFormat.DontCare);
+                imageList1.Images.Add(bitmap);
+            }
+
+            ListViewItem item = new ListViewItem(sVideoTime);
+            item.SubItems.Add(dVideoTime.ToString());
+            item.SubItems.Add("책갈피 " + sVideoTime);
+            item.ImageIndex = imageList1.Images.Count;
+
+            listViewBookmark.Items.Add(item);
+
+            //listBookmark.DisplayMember = "sVideoTime";
+            //listBookmark.Items.Add(item);          
+
+        }
+
+        private void listViewBookmark_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void listViewBookmark_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            if ((sender as ListView).SelectedItems.Count > 0)
+            {
+                string sVideoTime = (sender as ListView).SelectedItems[0].SubItems[1].Text;
+                double dVideoTime;
+                double.TryParse(sVideoTime, out dVideoTime);
+
+                axMediaPlayer1.Ctlcontrols.currentPosition = dVideoTime;
+
+                pictureBox1.Image = imageList1.Images[(sender as ListView).SelectedItems[0].Index];
+            }
+        }
+    }
+
+    internal class BookmarkItem
+    {
+        public BookmarkItem(string s, double d)
+        {
+            sTimeBookmark = s;
+            dTimeBookmark = d;
+        }
+        public string sTimeBookmark { get; set; }
+        public double dTimeBookmark { get; set; }
+    }
+}
